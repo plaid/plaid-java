1240dee0bcf97ac79b669aada054a87cf5df1f55
Michael Kelly <michael@plaid.com>
2017-07-27 17:45:58 -0700

    remove stray file

--- src/main/java/com/plaid/client/response/ApexProcessorTokenCreateResponse.java
+++
@@ -1,9 +0,0 @@
-package com.plaid.client.response;
-
-public final class ApexProcessorTokenCreateResponse extends BaseResponse {
-  private String processorToken;
-
-  public String getProcessorToken() {
-    return processorToken;
-  }
-}
