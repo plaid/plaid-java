71c81781627cc043c826f9d3e135ecd051622b8b
Arjun Puri <apuri@plaid.com>
2018-06-18 14:14:40 -0700

    [ASSETS-181] API definitions

--- src/main/java/com/plaid/client/PlaidApiService.java
+++ src/main/java/com/plaid/client/PlaidApiService.java
@@ -1,64 +1,10 @@
 package com.plaid.client;
 
-import com.plaid.client.request.AccountsBalanceGetRequest;
-import com.plaid.client.request.AccountsGetRequest;
-import com.plaid.client.request.AuthGetRequest;
-import com.plaid.client.request.CategoriesGetRequest;
-import com.plaid.client.request.CreditDetailsGetRequest;
-import com.plaid.client.request.IdentityGetRequest;
-import com.plaid.client.request.IncomeGetRequest;
-import com.plaid.client.request.InstitutionsGetByIdRequest;
-import com.plaid.client.request.InstitutionsGetRequest;
-import com.plaid.client.request.InstitutionsSearchRequest;
-import com.plaid.client.request.ItemAccessTokenInvalidateRequest;
-import com.plaid.client.request.ItemAccessTokenUpdateVersionRequest;
-import com.plaid.client.request.ItemCreateRequest;
-import com.plaid.client.request.ItemCredentialsEncryptRequest;
-import com.plaid.client.request.ItemCredentialsUpdateRequest;
-import com.plaid.client.request.ItemDeleteRequest;
-import com.plaid.client.request.ItemGetRequest;
-import com.plaid.client.request.ItemMfaEncryptRequest;
-import com.plaid.client.request.ItemMfaRequest;
-import com.plaid.client.request.ItemPublicTokenCreateRequest;
-import com.plaid.client.request.ItemPublicTokenExchangeRequest;
-import com.plaid.client.request.ItemRemoveRequest;
-import com.plaid.client.request.ItemStripeTokenCreateRequest;
-import com.plaid.client.request.ItemApexProcessorTokenCreateRequest;
-import com.plaid.client.request.ItemDwollaProcessorTokenCreateRequest;
-import com.plaid.client.request.ItemWebhookUpdateRequest;
-import com.plaid.client.request.SandboxItemResetLoginRequest;
-import com.plaid.client.request.SandboxPublicTokenCreateRequest;
-import com.plaid.client.request.TransactionsGetRequest;
-import com.plaid.client.response.AccountsBalanceGetResponse;
-import com.plaid.client.response.AccountsGetResponse;
-import com.plaid.client.response.AuthGetResponse;
-import com.plaid.client.response.CategoriesGetResponse;
-import com.plaid.client.response.CreditDetailsGetResponse;
-import com.plaid.client.response.IdentityGetResponse;
-import com.plaid.client.response.IncomeGetResponse;
-import com.plaid.client.response.InstitutionsGetByIdResponse;
-import com.plaid.client.response.InstitutionsGetResponse;
-import com.plaid.client.response.InstitutionsSearchResponse;
-import com.plaid.client.response.ItemAccessTokenInvalidateResponse;
-import com.plaid.client.response.ItemAccessTokenUpdateVersionResponse;
-import com.plaid.client.response.ItemCreateResponse;
-import com.plaid.client.response.ItemCredentialsEncryptResponse;
-import com.plaid.client.response.ItemCredentialsUpdateResponse;
-import com.plaid.client.response.ItemDeleteResponse;
-import com.plaid.client.response.ItemGetResponse;
-import com.plaid.client.response.ItemMfaEncryptResponse;
-import com.plaid.client.response.ItemMfaResponse;
-import com.plaid.client.response.ItemPublicTokenCreateResponse;
-import com.plaid.client.response.ItemPublicTokenExchangeResponse;
-import com.plaid.client.response.ItemRemoveResponse;
-import com.plaid.client.response.ItemStripeTokenCreateResponse;
-import com.plaid.client.response.ItemApexProcessorTokenCreateResponse;
-import com.plaid.client.response.ItemDwollaProcessorTokenCreateResponse;
-import com.plaid.client.response.ItemWebhookUpdateResponse;
-import com.plaid.client.response.SandboxItemResetLoginResponse;
-import com.plaid.client.response.SandboxPublicTokenCreateResponse;
-import com.plaid.client.response.TransactionsGetResponse;
+import com.plaid.client.request.*;
+import com.plaid.client.response.*;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
@@ -132,6 +78,25 @@ public interface PlaidApiService {
 
   @POST("/accounts/balance/get")
   Call<AccountsBalanceGetResponse> accountsBalanceGet(@Body AccountsBalanceGetRequest request);
+
+  @POST("/asset_report/create")
+  Call<AssetReportCreateResponse> assetReportCreate(@Body AssetReportCreateRequest request);
+
+  @POST("/asset_report/get")
+  Call<AssetReportGetResponse> assetReportGet(@Body AssetReportGetRequest request);
+
+  // This returns raw bytes so we don't have a wrapper class
+  @POST("/asset_report/pdf/get")
+  Call<ResponseBody> assetReportPdfGet(@Body AssetReportPdfGetRequest request);
+
+  @POST("/asset_report/remove")
+  Call<AssetReportRemoveResponse> assetReportRemove(@Body AssetReportRemoveRequest request);
+
+  @POST("/asset_report/audit_copy/create")
+  Call<AssetReportAuditCopyCreateResponse> assetReportAuditCopyCreate(@Body AssetReportAuditCopyCreateRequest request);
+
+  @POST("/asset_report/audit_copy/remove")
+  Call<AssetReportAuditCopyRemoveResponse> assetReportAuditCopyRemove(@Body AssetReportAuditCopyRemoveRequest request);
 
   @POST("/auth/get")
   Call<AuthGetResponse> authGet(@Body AuthGetRequest request);
