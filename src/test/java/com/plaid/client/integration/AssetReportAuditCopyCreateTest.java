6fc01c1d5ef31b6264c063c26647afcc8c39bc95
Arjun Puri <apuri@plaid.com>
2018-06-18 14:13:38 -0700

    [ASSETS-181] /asset_report/audit_copy/create endpoint

---
+++ src/test/java/com/plaid/client/integration/AssetReportAuditCopyCreateTest.java
@@ -0,0 +1,43 @@
+package com.plaid.client.integration;
+
+import com.plaid.client.PlaidClient;
+import com.plaid.client.request.AssetReportAuditCopyCreateRequest;
+import com.plaid.client.request.common.Product;
+import com.plaid.client.response.AssetReportAuditCopyCreateResponse;
+import com.plaid.client.response.AssetReportCreateResponse;
+import org.junit.Test;
+import retrofit2.Response;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+public class AssetReportAuditCopyCreateTest extends AbstractItemIntegrationTest {
+
+  @Override
+  protected List<Product> setupItemProducts() {
+    return Arrays.asList(Product.ASSETS);
+  }
+
+  @Override
+  protected String setupItemInstitutionId() {
+    return TARTAN_BANK_INSTITUTION_ID;
+  }
+
+  @Test
+  public void testAssetReportAuditCopyCreteSuccess() throws Exception {
+    // Create asset report to get a asset report token
+    PlaidClient client = client();
+    List<String> accessTokens = Arrays.asList(getItemCreateResponse().getAccessToken());
+    Response<AssetReportCreateResponse> createResponse = AssetReportCreateTest.createAssetReport(client, accessTokens);
+    String assetReportToken = createResponse.body().getAssetReportToken();
+
+    AssetReportGetTest.waitTillReady(client, assetReportToken);
+
+    AssetReportAuditCopyCreateRequest request = new AssetReportAuditCopyCreateRequest(assetReportToken, "fannie_mae");
+    Response<AssetReportAuditCopyCreateResponse> response = client.service().assetReportAuditCopyCreate(request).execute();
+    assertNotNull(response.body().getAuditCopyToken());
+  }
+
+}
