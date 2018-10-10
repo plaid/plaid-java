d02e9874ce86018b467a445303c6cda8d89125ec
Stephen Tsai <stsai@plaid.com>
2018-10-09 20:03:37 -0700

    Fix AssetReportFilterTest

--- src/test/java/com/plaid/client/integration/AssetReportFilterTest.java
+++ src/test/java/com/plaid/client/integration/AssetReportFilterTest.java
@@ -21,74 +21,74 @@ import static org.junit.Assert.assertNotNull;
 
 public class AssetReportFilterTest extends AbstractItemIntegrationTest {
 
-    @Override
-    protected List<Product> setupItemProducts() {
-        return Arrays.asList(Product.ASSETS);
+  @Override
+  protected List<Product> setupItemProducts() {
+    return Arrays.asList(Product.ASSETS);
+  }
+
+  private Response<AssetReportCreateResponse> filterAssetReport(
+      AssetReportFilterRequest assetReportFilterRequest) throws Exception {
+    PlaidClient client = client();
+    Response<AssetReportCreateResponse> response =
+        client
+            .service()
+            .assetReportFilter(assetReportFilterRequest)
+            .execute();
+
+    return response;
+  }
+
+  @Override
+  protected String setupItemInstitutionId() {
+    return TARTAN_BANK_INSTITUTION_ID;
+  }
+
+  @Test
+  public void testAssetReportFilterSuccess() throws Exception {
+    // Create asset report to get an asset report token
+    List<String> accessTokens = Arrays.asList(getItemCreateResponse().getAccessToken());
+    Response<AssetReportCreateResponse> createResponse = AssetReportCreateTest.createAssetReport(client(), accessTokens);
+    String assetReportToken = createResponse.body().getAssetReportToken();
+
+    // Wait till asset report is ready
+    Response<AssetReportGetResponse> response = waitTillReady(client(), assetReportToken);
+
+    // Validate the responses
+    AssetReportGetResponse respBody = response.body();
+    assertSuccessResponse(response);
+
+    assertNotNull(respBody.getReport());
+    assertNotNull(respBody.getReport().getUser());
+    assertFalse(respBody.getReport().getItems().isEmpty());
+
+    assertNotNull(respBody.getReport().getAssetReportId());
+
+    List<String> accountIdsToExclude = new ArrayList<>();
+    accountIdsToExclude.add(respBody.getReport().getItems().get(0).getAccounts().get(0).getAccountId());
+
+    // create a filtered report
+    AssetReportFilterRequest assetReportFilterRequest = new AssetReportFilterRequest(assetReportToken, accountIdsToExclude);
+
+    Response<AssetReportCreateResponse> assetReportFilterResponse = filterAssetReport(assetReportFilterRequest);
+
+    String assetReportFilterToken = assetReportFilterResponse.body().getAssetReportToken();
+
+    // wait till the filtered asset reponse is ready
+    Response<AssetReportGetResponse> filteredReportResponse = waitTillReady(client(), assetReportFilterToken);
+
+    assertEquals(getAssetReportAccountIds(respBody.getReport()).size() - 1,
+        getAssetReportAccountIds(filteredReportResponse.body().getReport()).size());
+  }
+
+  private List<String> getAssetReportAccountIds(AssetReportGetResponse.AssetReport assetReport) {
+    List<String> assetReportAccounts = new ArrayList<>();
+
+    for (AssetReportGetResponse.Item item : assetReport.getItems()) {
+      for (AssetReportGetResponse.Account account : item.getAccounts()) {
+        assetReportAccounts.add(account.getAccountId());
+      }
     }
-
-    private Response<AssetReportCreateResponse> filterAssetReport(
-            AssetReportFilterRequest assetReportFilterRequest) throws Exception {
-        PlaidClient client = client();
-        Response<AssetReportCreateResponse> response =
-                client
-                        .service()
-                        .assetReportFilter(assetReportFilterRequest)
-                        .execute();
-
-        return response;
-    }
-
-    @Override
-    protected String setupItemInstitutionId() {
-        return TARTAN_BANK_INSTITUTION_ID;
-    }
-
-    @Test
-    public void testAssetReportCreateSuccess() throws Exception {
-        // Create asset report to get an asset report token
-        List<String> accessTokens = Arrays.asList(getItemCreateResponse().getAccessToken());
-        Response<AssetReportCreateResponse> createResponse = AssetReportCreateTest.createAssetReport(client(), accessTokens);
-        String assetReportToken = createResponse.body().getAssetReportToken();
-
-        // Wait till asset report is ready
-        Response<AssetReportGetResponse> response = waitTillReady(client(), assetReportToken);
-
-        // Validate the responses
-        AssetReportGetResponse respBody = response.body();
-        assertSuccessResponse(response);
-
-        assertNotNull(respBody.getReport());
-        assertNotNull(respBody.getReport().getUser());
-        assertFalse(respBody.getReport().getItems().isEmpty());
-
-        assertNotNull(respBody.getReport().getAssetReportId());
-
-        List<String> accountIdsToExclude = new ArrayList<>();
-        accountIdsToExclude.add(respBody.getReport().getItems().get(0).getAccounts().get(0).getAccountId());
-
-        // create a filtered report
-        AssetReportFilterRequest assetReportFilterRequest = new AssetReportFilterRequest(assetReportToken, accountIdsToExclude);
-
-        Response<AssetReportCreateResponse> assetReportFilterResponse = filterAssetReport(assetReportFilterRequest);
-
-        String assetReportFilterToken = assetReportFilterResponse.body().getAssetReportToken();
-
-        // wait till the filtered asset reponse is ready
-        Response<AssetReportGetResponse> filteredReportResponse = waitTillReady(client(), assetReportFilterToken);
-
-        assertEquals(getAssetReportAccountIds(filteredReportResponse.body().getReport()).size(),
-                getAssetReportAccountIds(respBody.getReport()).size());
-    }
-
-    private List<String> getAssetReportAccountIds(AssetReportGetResponse.AssetReport assetReport) {
-        List<String> assetReportAccounts = new ArrayList<>();
-
-        for (AssetReportGetResponse.Item item : assetReport.getItems()) {
-            for (AssetReportGetResponse.Account account : item.getAccounts()) {
-                assetReportAccounts.add(account.getAccountId());
-            }
-        }
-        return assetReportAccounts;
-    }
+    return assetReportAccounts;
+  }
 }
 
