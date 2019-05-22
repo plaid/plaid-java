20e9ce93a34bb26373e2403b3bcc64c2bda25801
Hamel Ajay Kothari <hkothari@hydronitrogen.com>
2019-05-22 14:41:15 -0400

    Adding support for Item status info

--- src/main/java/com/plaid/client/response/ItemGetResponse.java
+++ src/main/java/com/plaid/client/response/ItemGetResponse.java
@@ -2,8 +2,13 @@ package com.plaid.client.response;
 
 public final class ItemGetResponse extends BaseResponse {
   private ItemStatus item;
+  private ItemStatusStatus status;
 
   public ItemStatus getItem() {
     return item;
+  }
+
+  public ItemStatusStatus getStatus() {
+    return status;
   }
 }
