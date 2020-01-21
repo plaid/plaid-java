3561abcf829763cfd2c0f8a2afa602010a791f7d
skansal <skansal@plaid.com>
2020-01-21 20:05:07 +0530

    adds recipient id to recipient model

--- src/main/java/com/plaid/client/model/payment/Recipient.java
+++ src/main/java/com/plaid/client/model/payment/Recipient.java
@@ -3,14 +3,13 @@ package com.plaid.client.model.payment;
 import com.plaid.client.model.payment.Address;
 
 public final class Recipient {
+  private String recipientId;
   private String name;
   private String iban;
   private Address address;
 
-  Recipient(String name, String iban, Address address) {
-    this.name = name;
-    this.iban = iban;
-    this.address = address;
+  public String getRecipientId() {
+    return recipientId;
   }
 
   public String getName() {
