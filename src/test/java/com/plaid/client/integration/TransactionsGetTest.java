af4b07c0d9daf57608dc8f2665e443ca29c82de0
Andrew Chen <andrewchen@plaid.com>
2020-02-03 11:09:44 -0800

    Add authorized_date field to transactions

--- src/test/java/com/plaid/client/integration/TransactionsGetTest.java
+++ src/test/java/com/plaid/client/integration/TransactionsGetTest.java
@@ -80,6 +80,7 @@ public class TransactionsGetTest extends AbstractIntegrationTest {
       assertNotNull(txn.getAmount());
       assertNotNull(txn.getLocation());
       assertNotNull(txn.getIsoCurrencyCode());
+      assertNotNull(txn.getAuthorizedDate());
     }
   }
 
