497f781c2ce8d83bc02d72b53516e5f132bb9409
Jack Siegel <jack@plaid.com>
2020-02-26 15:23:16 +0000

    Fix compilation error

--- src/test/java/com/plaid/client/integration/InstitutionsSearchTest.java
+++ src/test/java/com/plaid/client/integration/InstitutionsSearchTest.java
@@ -15,6 +15,7 @@ import static junit.framework.TestCase.assertNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class InstitutionsSearchTest extends AbstractIntegrationTest {
   @Test
