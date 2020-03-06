f5b5d35a449fe15f605c8c3ef173764815b66058
Aaron Walker <awwalker3@gmail.com>
2020-03-06 12:24:04 -0500

    add investments to item/get/ test products (#200)
    Co-authored-by: Aaron Walker <awalker@plaid.com>

--- src/test/java/com/plaid/client/integration/ItemGetTest.java
+++ src/test/java/com/plaid/client/integration/ItemGetTest.java
@@ -8,7 +8,7 @@ import com.plaid.client.response.ItemStatusStatus;
 import org.junit.Test;
 import retrofit2.Response;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertNull;
@@ -16,7 +16,7 @@ import static org.junit.Assert.assertNull;
 public class ItemGetTest extends AbstractItemIntegrationTest {
   @Override
   protected List<Product> setupItemProducts() {
-    return Collections.singletonList(Product.TRANSACTIONS);
+    return Arrays.asList(Product.TRANSACTIONS, Product.INVESTMENTS);
   }
 
   @Override
