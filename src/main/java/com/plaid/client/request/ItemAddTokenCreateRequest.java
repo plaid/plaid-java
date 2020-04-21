4e6b2985ebab1832e329cb50bf37b7456e499a43
Jayme Woogerd <jwoogerd@plaid.com>
2020-04-21 12:51:17 -0400

    Fix syntax

--- src/main/java/com/plaid/client/request/ItemAddTokenCreateRequest.java
+++ src/main/java/com/plaid/client/request/ItemAddTokenCreateRequest.java
@@ -24,21 +24,21 @@ public final class ItemAddTokenCreateRequest extends BaseClientRequest {
       this.clientUserId = clientUserId;
     }
 
-    public User withLegalName(String legalName)
+    public User withLegalName(String legalName) {
       notNull(legalName, "legalName");
       options = options.orElse(new Options());
       userOptions.get().legalName = legalName;
       return this;
     }
 
-    public User withPhoneNumber(String phoneNumber)
+    public User withPhoneNumber(String phoneNumber) {
       notNull(phoneNumber, "phoneNumber");
       options = options.orElse(new Options());
       options.get().phoneNumber = phoneNumber;
       return this;
     }
 
-    public User withVerifiedPhoneNumber(String phoneNumber, Date verifiedTime)
+    public User withVerifiedPhoneNumber(String phoneNumber, Date verifiedTime) {
       notNull(phoneNumber, "phoneNumber");
       notNull(verifiedTime, "verifiedTime");
       options = options.orElse(new Options());
@@ -47,14 +47,14 @@ public final class ItemAddTokenCreateRequest extends BaseClientRequest {
       return this;
     }
 
-    public User withEmailAddress(String emailAddress)
+    public User withEmailAddress(String emailAddress) {
       notNull(emailAddress, "emailAddress");
       options = options.orElse(new Options());
       options.get().emailAddress = emailAddress;
       return this;
     }
 
-    public User withVerifiedEmailAddress(String emailAddress, Date verifiedTime)
+    public User withVerifiedEmailAddress(String emailAddress, Date verifiedTime) {
       notNull(emailAddress, "emailAddress");
       notNull(verifiedTime, "verifiedTime");
       options = options.orElse(new Options());
