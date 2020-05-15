148b36b6aad5c5f956528dc7eb5fc96670835859
Andrew Coover <acoover@gmail.com>
2020-05-15 09:11:59 -0700

    set verification status (#214)
    Co-authored-by: Andrew Coover <acoover@plaid.com>

---
+++ src/main/java/com/plaid/client/response/SandboxItemSetVerificationStatusResponse.java
@@ -0,0 +1,6 @@
+package com.plaid.client.response;
+
+/**
+ * Response from /sandbox/item/set_verification_status endpoint.
+ */
+public final class SandboxItemSetVerificationStatusResponse extends BaseResponse {}
