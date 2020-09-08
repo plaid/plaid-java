2db91c169a06f128c87ab63db0e48440789290bd
David Zhang <dzhang@plaid.com>
2020-09-08 18:30:27 -0400

    lint

--- src/main/java/com/plaid/client/request/LinkTokenCreateRequest.java
+++ src/main/java/com/plaid/client/request/LinkTokenCreateRequest.java
@@ -45,10 +45,14 @@ public class LinkTokenCreateRequest extends BaseClientRequest {
       this.accountSubtypes = accountSubtypes;
     }
 
-    @Override public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      SubtypeFilters that = (SubtypeFilters) o;
+    @Override public boolean equals(Object other) {
+      if (this == other) {
+        return true;
+      }
+      if (other == null || getClass() != other.getClass()) {
+        return false;
+      }
+      SubtypeFilters that = (SubtypeFilters) other;
       return Objects.equals(accountSubtypes, that.accountSubtypes);
     }
 
