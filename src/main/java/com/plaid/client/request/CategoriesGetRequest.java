929b63a9653bb07536dc3cd234e345b9c49e43af
Chase Farmer <cfarmer@plaid.com>
2021-02-19 09:18:21 -1000

    OpenAPI generated code at 02_19_21_09_18_17

--- src/main/java/com/plaid/client/request/CategoriesGetRequest.java
+++
@@ -1,9 +0,0 @@
-package com.plaid.client.request;
-
-/**
- * A request object for /categories/get .
- *
- * Mostly pointless. Just need to get gson to render an empty object.
- */
-public class CategoriesGetRequest {
-}
--- src/test/java/com/plaid/client/integration/AssetReportRemoveTest.java
+++ src/test/java/com/plaid/client/integration/AssetReportRemoveTest.java
@@ -1,23 +1,21 @@
 package com.plaid.client.integration;
 
-import com.plaid.client.PlaidClient;
-import com.plaid.client.request.AssetReportRemoveRequest;
-import com.plaid.client.request.common.Product;
-import com.plaid.client.response.AssetReportCreateResponse;
-import com.plaid.client.response.AssetReportRemoveResponse;
-import org.junit.Test;
-import retrofit2.Response;
+import static org.junit.Assert.assertTrue;
 
+import com.plaid.client.model.AssetReportCreateResponse;
+import com.plaid.client.model.AssetReportRemoveRequest;
+import com.plaid.client.model.AssetReportRemoveResponse;
+import com.plaid.client.model.Products;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import retrofit2.Response;
 
 public class AssetReportRemoveTest extends AbstractItemIntegrationTest {
 
   @Override
-  protected List<Product> setupItemProducts() {
-    return Arrays.asList(Product.ASSETS);
+  protected List<Products> setupItemProducts() {
+    return Arrays.asList(Products.ASSETS);
   }
 
   @Override
@@ -28,19 +26,24 @@ public class AssetReportRemoveTest extends AbstractItemIntegrationTest {
   @Test
   public void testAssetReportRemoveSuccess() throws Exception {
     // Create asset report to get an asset report token
-    PlaidClient client = client();
-    List<String> accessTokens = Arrays.asList(getItemPublicTokenExchangeResponse().getAccessToken());
-    Response<AssetReportCreateResponse> createResponse = AssetReportCreateTest.createAssetReport(client, accessTokens);
+    List<String> accessTokens = Arrays.asList(
+      getItemPublicTokenExchangeResponse().getAccessToken()
+    );
+    Response<AssetReportCreateResponse> createResponse = AssetReportCreateTest.createAssetReport(
+      client(),
+      accessTokens
+    );
     String assetReportToken = createResponse.body().getAssetReportToken();
 
     // Poll Plaid till report is ready
-    AssetReportGetTest.waitTillReady(client, assetReportToken);
+    AssetReportGetTest.waitTillReady(client(), assetReportToken);
+    AssetReportRemoveRequest assetReportRemoveRequest = new AssetReportRemoveRequest()
+      .assetReportToken(assetReportToken);
 
-    Response<AssetReportRemoveResponse> response =
-      client
-        .service()
-        .assetReportRemove(new AssetReportRemoveRequest(assetReportToken))
-        .execute();
-    assertTrue(response.body().isRemoved());
+    Response<AssetReportRemoveResponse> response = client()
+      .assetReportRemove(assetReportRemoveRequest)
+      .execute();
+
+    assertTrue(response.body().getRemoved());
   }
 }
