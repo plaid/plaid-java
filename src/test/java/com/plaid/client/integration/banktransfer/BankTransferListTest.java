04c7f7483d4aabde9b7e727d723d558ee8cfc778
Chase Farmer <cfarmer@plaid.com>
2021-02-24 13:29:17 -1000

    correct file this time

--- src/test/java/com/plaid/client/integration/banktransfer/BankTransferListTest.java
+++ src/test/java/com/plaid/client/integration/banktransfer/BankTransferListTest.java
@@ -2,36 +2,18 @@ package com.plaid.client.integration.banktransfer;
 
 import static org.junit.Assert.assertEquals;
 
-import com.plaid.client.model.banktransfer.BankTransferEvent;
-import com.plaid.client.request.SandboxBankTransferSimulateRequest;
-import com.plaid.client.request.banktransfer.BankTransferEventListRequest;
-import com.plaid.client.response.SandboxBankTransferSimulateResponse;
-import com.plaid.client.response.banktransfer.BankTransferEventListResponse;
-
-import org.junit.Before;
-import java.util.List;
+import com.plaid.client.request.banktransfer.BankTransferListRequest;
+import com.plaid.client.response.banktransfer.BankTransferListResponse;
 
 import retrofit2.Response;
 
-public class BankTransferEventListTest extends AbstractBankTransferTest {
-  @Before
-  public void simulatePosted() throws AssertionError, Exception {
-    Response<SandboxBankTransferSimulateResponse> simulateResponse = client().service().sandboxBankTransferSimulate(
-      new SandboxBankTransferSimulateRequest(getBankTransfer().getId(), "posted")
-    ).execute();
-    assertSuccessResponse(simulateResponse);
-  }
-
+public class BankTransferListTest extends AbstractBankTransferTest {
   @Override
   protected void bankTransferTest() throws AssertionError, Exception {
-    Response<BankTransferEventListResponse> eventListResponse = client().service().bankTransferEventList(
-      new BankTransferEventListRequest().withBankTransferId(getBankTransfer().getId())
+    Response<BankTransferListResponse> response = client().service().bankTransferList(
+      new BankTransferListRequest().withCount(1)
     ).execute();
-    assertSuccessResponse(eventListResponse);
-    List<BankTransferEvent> bankTransferEvents = eventListResponse.body().getBankTransferEvents();
-    assertEquals(2, bankTransferEvents.size());
-    for (BankTransferEvent e : bankTransferEvents) {
-      assertEquals(getBankTransfer().getId(), e.getBankTransferId());
-    }
+    assertSuccessResponse(response);
+    assertEquals(1, response.body().getBankTransfers().size());
   }
 };
