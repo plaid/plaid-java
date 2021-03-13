897d2873f341a9f2e4661e4d2b0f9fbece7c583a
smaronesy <smaronesy@plaid.com>
2021-03-12 20:08:41 -0800

    [DEVX-1425] added mortgage object to Liabilities

--- src/main/java/com/plaid/client/response/LiabilitiesGetResponse.java
+++ src/main/java/com/plaid/client/response/LiabilitiesGetResponse.java
@@ -20,16 +20,185 @@ public class LiabilitiesGetResponse extends BaseResponse {
   }
 
   public static class Liabilities {
-    private List<StudentLoanLiability> student;
     private List<CreditCardLiability> credit;
+    private List<StudentLoanLiability> student;
+    private  List<MortgageLiability> mortgage;
+
+    public List<CreditCardLiability> getCredit() {
+      return credit;
+    }
 
     public List<StudentLoanLiability> getStudent() {
       return student;
     }
 
-    public List<CreditCardLiability> getCredit() {
-      return credit;
+    public List<MortgageLiability> getMortgage() {
+      return mortgage;
     }
+  }
+
+  // CreditCardLiability contains credit card liability data.
+  public static class CreditCardLiability {
+    private List<Apr> aprs;
+    private String accountId;
+    private Boolean isOverdue;
+    private Double lastPaymentAmount;
+    private String lastPaymentDate;
+    private Double lastStatementBalance;
+    private String lastStatementIssueDate;
+    private Double minimumPaymentAmount;
+    private String nextPaymentDueDate;
+
+    public List<Apr> getAprs() {
+      return aprs;
+    }
+
+    public String getAccountId() {
+      return accountId;
+    }
+
+    public Boolean getIsOverdue() {
+      return isOverdue;
+    }
+
+    public Double getLastPaymentAmount() {
+      return lastPaymentAmount;
+    }
+
+    public String getLastPaymentDate() {
+      return lastPaymentDate;
+    }
+
+    public Double getLastStatementBalance() {
+      return lastStatementBalance;
+    }
+
+    public String getLastStatementIssueDate() {
+      return lastStatementIssueDate;
+    }
+
+    public Double getMinimumPaymentAmount() {
+      return minimumPaymentAmount;
+    }
+
+    public String getNextPaymentDueDate() {
+      return nextPaymentDueDate;
+    }
+  }
+
+  // Apr contains APR data related to credit card liability.
+  public static class Apr {
+    private Double aprPercentage;
+    private String aprType;
+    private Double balanceSubjectToApr;
+    private Double interestChargeAmount;
+
+    public Double getAprPercentage() {
+      return aprPercentage;
+    }
+
+    public String getAprType() {
+      return aprType;
+    }
+
+    public Double getBalanceSubjectToApr() {
+      return balanceSubjectToApr;
+    }
+
+    public Double getInterestChargeAmount() {
+      return interestChargeAmount;
+    }
+  }
+
+  // MortgageLiability contains mortgage liability data.
+  public static class MortgageLiability {
+    private String accountId;
+    private String accountNumber;
+    private Double currentLateFee;
+    private Double escrowBalance;
+    private Boolean hasPmi;
+    private Boolean hasPrepaymentPenalty;
+    private InterestRate interestRate;
+    private Double lastPaymentAmount;
+    private String lastPaymentDate;
+    private String loanTypeDescription;
+    private String loanTerm;
+    private String maturityDate;
+    private Double nextMonthlyPayment;
+    private String nextPaymentDueDate;
+    private String originationDate;
+    private Double originationPrincipalAmount;
+    private Double pastDueAmount;
+    private PropertyAddress propertyAddress;
+    private Double ytdInterestPaid;
+    private Double ytdPrincipalPaid;
+
+    public String getAccountId() { return accountId; }
+
+    public String getAccountNumber() { return accountNumber; }
+
+    public Double getCurrentLateFee() { return currentLateFee; }
+
+    public Double getEscrowBalance() { return escrowBalance; }
+
+    public Boolean getHasPmi() { return hasPmi; }
+
+    public Boolean getHasPrepaymentPenalty() { return hasPrepaymentPenalty; }
+
+    public InterestRate getInterestRate() { return interestRate; }
+
+    public Double getLastPaymentAmount() { return lastPaymentAmount; }
+
+    public String getLastPaymentDate() { return lastPaymentDate; }
+
+    public String getLoanTypeDescription() { return loanTypeDescription; }
+
+    public String getLoanTerm() { return loanTerm; }
+
+    public String getMaturityDate() { return maturityDate; }
+
+    public Double getNextMonthlyPayment() { return nextMonthlyPayment; }
+
+    public String getNextPaymentDueDate() { return nextPaymentDueDate; }
+
+    public String getOriginationDate() { return originationDate; }
+
+    public Double getOriginationPrincipalAmount() { return originationPrincipalAmount; }
+
+    public Double getPastDueAmount() { return pastDueAmount; }
+
+    public PropertyAddress getPropertyAddress() { return propertyAddress; }
+
+    public Double getYtdInterestPaid() { return ytdInterestPaid; }
+
+    public Double getYtdPrincipalPaid() { return ytdPrincipalPaid; }
+  }
+
+  public static class InterestRate {
+    private Double percentage;
+    private String type;
+
+    public Double getPercentage() { return percentage; }
+
+    public String getType() { return type; }
+  }
+
+  public static class PropertyAddress {
+    private String city;
+    private String country;
+    private String postalCode;
+    private String region;
+    private String street;
+
+    public String getCity() { return city; }
+
+    public String getCountry() { return country; }
+
+    public String getPostalCode() { return postalCode; }
+
+    public String getRegion() { return region; }
+
+    public String getStreet() { return street; }
   }
 
   // StudentLoanLiability contains student loan liability data.
@@ -236,79 +405,6 @@ public class LiabilitiesGetResponse extends BaseResponse {
 
     public String getDescription() {
       return description;
-    }
-  }
-
-  // CreditCardLiability contains credit card liability data.
-  public static class CreditCardLiability {
-    private List<Apr> aprs;
-    private String accountId;
-    private Boolean isOverdue;
-    private Double lastPaymentAmount;
-    private String lastPaymentDate;
-    private Double lastStatementBalance;
-    private String lastStatementIssueDate;
-    private Double minimumPaymentAmount;
-    private String nextPaymentDueDate;
-
-    public List<Apr> getAprs() {
-      return aprs;
-    }
-
-    public String getAccountId() {
-      return accountId;
-    }
-
-    public Boolean getIsOverdue() {
-      return isOverdue;
-    }
-
-    public Double getLastPaymentAmount() {
-      return lastPaymentAmount;
-    }
-
-    public String getLastPaymentDate() {
-      return lastPaymentDate;
-    }
-
-    public Double getLastStatementBalance() {
-      return lastStatementBalance;
-    }
-
-    public String getLastStatementIssueDate() {
-      return lastStatementIssueDate;
-    }
-
-    public Double getMinimumPaymentAmount() {
-      return minimumPaymentAmount;
-    }
-
-    public String getNextPaymentDueDate() {
-      return nextPaymentDueDate;
-    }
-  }
-
-  // Apr contains APR data related to credit card liability.
-  public static class Apr {
-    private Double aprPercentage;
-    private String aprType;
-    private Double balanceSubjectToApr;
-    private Double interestChargeAmount;
-
-    public Double getAprPercentage() {
-      return aprPercentage;
-    }
-
-    public String getAprType() {
-      return aprType;
-    }
-
-    public Double getBalanceSubjectToApr() {
-      return balanceSubjectToApr;
-    }
-
-    public Double getInterestChargeAmount() {
-      return interestChargeAmount;
     }
   }
 }
