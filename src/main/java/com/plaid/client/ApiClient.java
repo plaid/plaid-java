acce264ea51e73a6b8a5a273b0f2041b5d387680
Li Zhang <li3zhang@gmail.com>
2024-11-14 13:25:04 -0500

    OpenAPI generated code at 2024-11-14T16:42:10Z (#434)
    Co-authored-by: lzhang <lzhang@plaid.com>

--- src/main/java/com/plaid/client/ApiClient.java
+++ src/main/java/com/plaid/client/ApiClient.java
@@ -49,7 +49,7 @@ public class ApiClient {
             public Response intercept(Chain chain) throws IOException {
                 Request originalRequest = chain.request();
                 Request requestWithUserAgent = originalRequest.newBuilder()
-                        .header("User-Agent", "Plaid Java v28.0.0")
+                        .header("User-Agent", "Plaid Java v29.0.0")
                         .header("Plaid-Version", "2020-09-14")
                         .build();
                 return chain.proceed(requestWithUserAgent);
